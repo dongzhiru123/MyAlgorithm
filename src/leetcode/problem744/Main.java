cf5cc96fa0e5dd5c7bb65aef5379e83063b89a8f
dzr <2201357779@qq.com>
2020-06-11 11:25:47 +0800

    LeetCode 744

---
+++ src/leetcode/problem744/Main.java
@@ -0,0 +1,60 @@
+package leetcode.problem744;
+
+/**
+ * 给你一个排序后的字符列表 letters ，列表中只包含小写英文字母。另给出一个目标字母 target，请你寻找在这一有序列表里比目标字母大的最小字母。
+ *
+ * 在比较时，字母是依序循环出现的。举个例子：
+ *
+ * 如果目标字母 target = 'z' 并且字符列表为 letters = ['a', 'b']，则答案返回 'a'
+ *
+ * 示例：
+ *
+ * 输入:
+ * letters = ["c", "f", "j"]
+ * target = "a"
+ * 输出: "c"
+ *
+ * 输入:
+ * letters = ["c", "f", "j"]
+ * target = "c"
+ * 输出: "f"
+ *
+ * 输入:
+ * letters = ["c", "f", "j"]
+ * target = "d"
+ * 输出: "f"
+ *
+ * 输入:
+ * letters = ["c", "f", "j"]
+ * target = "g"
+ * 输出: "j"
+ *
+ * 输入:
+ * letters = ["c", "f", "j"]
+ * target = "j"
+ * 输出: "c"
+ *
+ * 输入:
+ * letters = ["c", "f", "j"]
+ * target = "k"
+ * 输出: "c"
+ */
+public class Main {
+    public static void main(String[] args) {
+        char[] letters = {'c', 'f', 'j'};
+        System.out.println(nextGreatestLetter(letters, 'c'));
+    }
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int length = letters.length;
+        int lo = 0, hi = length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (letters[mid] <= target) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        return lo < length ? letters[lo] : letters[0];
+    }
+}
