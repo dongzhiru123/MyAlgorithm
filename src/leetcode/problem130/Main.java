f6e7b9ac8065b759db5d78d1fb3f8fb7bd248f83
dzr <2201357779@qq.com>
2020-06-16 10:34:22 +0800

    LeetCode 130

---
+++ src/leetcode/problem130/Main.java
@@ -0,0 +1,72 @@
+package leetcode.problem130;
+
+/**
+ * 给定一个二维的矩阵，包含 'X' 和 'O'（字母 O）。
+ *
+ * 找到所有被 'X' 围绕的区域，并将这些区域里所有的 'O' 用 'X' 填充。
+ *
+ * 示例:
+ * X X X X
+ * X O O X
+ * X X O X
+ * X O X X
+ *
+ * 运行你的函数后，矩阵变为：
+ * X X X X
+ * X X X X
+ * X X X X
+ * X O X X
+ * 解释:
+ * 被围绕的区间不会存在于边界上，换句话说，任何边界上的 'O' 都不会被填充为 'X'。 任何不在边界上，或不与边界上的 'O' 相连的 'O' 最终都会被填充为 'X'。如果两个元素在水平或垂直方向相邻，则称它们是“相连”的。
+ */
+public class Main {
+    public static void main(String[] args) {
+        char[][] board = {
+                {'X', 'X', 'X', 'X'},
+                {'X', 'O', 'O', 'X'},
+                {'X', 'X', 'O', 'X'},
+                {'X', 'O', 'X', 'X'}
+        };
+        solve(board);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void solve(char[][] board) {
+        if (board == null || board.length == 0 || board[0].length == 0) {
+            return;
+        }
+        int m = board.length;
+        int n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, n - 1);
+        }
+        for (int i = 0; i < n; i++) {
+            dfs(board, 0, i);
+            dfs(board, m - 1, i);
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                } else if (board[i][j] == 'T') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+    }
+    private static void dfs(char[][] board, int m, int n) {
+        if (m < 0 || m >= board.length || n < 0 || n >= board[0].length || board[m][n] != 'O') {
+            return;
+        }
+        board[m][n] = 'T';
+        dfs(board, m - 1, n);
+        dfs(board, m + 1, n);
+        dfs(board, m, n - 1);
+        dfs(board, m, n + 1);
+    }
+}
