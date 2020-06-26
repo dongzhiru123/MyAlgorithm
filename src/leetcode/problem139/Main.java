93d3156ac4e8a49f5c52681982dd970ce4f71085
dzr <2201357779@qq.com>
2020-06-26 11:43:37 +0800

    leetcode 139

---
+++ src/leetcode/problem139/Main.java
@@ -0,0 +1,55 @@
+package leetcode.problem139;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+ *
+ * 说明：
+ *
+ * 拆分时可以重复使用字典中的单词。
+ * 你可以假设字典中没有重复的单词。
+ *
+ * 示例 1：
+ * 输入: s = "leetcode", wordDict = ["leet", "code"]
+ * 输出: true
+ * 解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+ *
+ * 示例 2：
+ * 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+ * 输出: true
+ * 解释: 返回 true 因为 "applepenapple" 可以被拆分成 "apple pen apple"。
+ *      注意你可以重复使用字典中的单词。
+ *
+ * 示例 3：
+ * 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * 输出: false
+ */
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println(main.wordBreak("leetcode", new ArrayList<String>() {{
+            add("leet");
+            add("code");
+        }}));
+    }
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if (s == null) {
+            return false;
+        }
+        int length = s.length();
+        boolean[] dp = new boolean[length + 1];
+        dp[0] = true;
+        for (int i = 1; i <= length; i++) {
+            for (String string : wordDict) {
+                int stringLength = string.length();
+                if (stringLength <= i && string.equals(s.substring(i - stringLength, i))) {
+                    dp[i] = dp[i]
+                            || dp[i - stringLength];
+                }
+            }
+        }
+        return dp[length];
+    }
+}
