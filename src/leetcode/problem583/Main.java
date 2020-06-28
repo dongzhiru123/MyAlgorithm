40e01db631312da6ad126da5aa5927ca99cb2892
dzr <2201357779@qq.com>
2020-06-28 11:33:10 +0800

    leetcode 583

---
+++ src/leetcode/problem583/Main.java
@@ -0,0 +1,36 @@
+package leetcode.problem583;
+
+/**
+ *给定两个单词 word1 和 word2，找到使得 word1 和 word2 相同所需的最小步数，每步可以删除任意一个字符串中的一个字符。
+ *
+ * 示例：
+ *
+ * 输入: "sea", "eat"
+ * 输出: 2
+ * 解释: 第一步将"sea"变为"ea"，第二步将"eat"变为"ea"
+ *  
+ * 提示：
+ * 给定单词的长度不超过500。
+ * 给定单词中的字符只含有小写字母。
+ */
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println(main.minDistance("sea", "eat"));
+    }
+    public int minDistance(String word1, String word2) {
+        int length1 = word1.length();
+        int length2 = word2.length();
+        int[][] dp = new int[length1 + 1][length2 + 1];
+        for (int i = 1; i <= length1; i++) {
+            for (int j = 1; j <= length2; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return length1 + length2 - 2 * dp[length1][length2];
+    }
+}
