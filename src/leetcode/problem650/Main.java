ff93531d4f9bf0d8a6a0bd5e96259cf271aa0aec
dzr <2201357779@qq.com>
2020-06-28 13:25:55 +0800

    leetcode 650

---
+++ src/leetcode/problem650/Main.java
@@ -0,0 +1,41 @@
+package leetcode.problem650;
+
+/**
+ *最初在一个记事本上只有一个字符 'A'。你每次可以对这个记事本进行两种操作：
+ *
+ * Copy All (复制全部) : 你可以复制这个记事本中的所有字符(部分的复制是不允许的)。
+ * Paste (粘贴) : 你可以粘贴你上一次复制的字符。
+ * 给定一个数字 n 。你需要使用最少的操作次数，在记事本中打印出恰好 n 个 'A'。输出能够打印出 n 个 'A' 的最少操作次数。
+ *
+ * 示例 1:
+ * 输入: 3
+ * 输出: 3
+ * 解释:
+ * 最初, 我们只有一个字符 'A'。
+ * 第 1 步, 我们使用 Copy All 操作。
+ * 第 2 步, 我们使用 Paste 操作来获得 'AA'。
+ * 第 3 步, 我们使用 Paste 操作来获得 'AAA'。
+ *
+ * 说明:
+ * n 的取值范围是 [1, 1000] 。
+ */
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println(main.minSteps(3));
+    }
+    public int minSteps(int n) {
+        int temp = (int) Math.sqrt(n);
+        int[] dp = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            dp[i] = i;
+            for (int j = 2; j <= temp; j++) {
+                if (i % j == 0) {
+                    dp[i] = dp[j] + dp[i / j];
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
