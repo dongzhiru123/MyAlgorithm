4e8ca6901876f17153f308049bca7915d891b729
dzr <2201357779@qq.com>
2020-07-06 13:58:50 +0800

    leetcode 234

---
+++ src/leetcode/problem234/Main.java
@@ -0,0 +1,70 @@
+package leetcode.problem234;
+
+/**
+ *请判断一个链表是否为回文链表。
+ *
+ * 示例 1:
+ * 输入: 1->2
+ * 输出: false
+ *
+ * 示例 2:
+ * 输入: 1->2->2->1
+ * 输出: true
+ * 进阶：
+ * 你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+ */
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        ListNode listNode = new ListNode(2);
+        listNode.next = new ListNode(3);
+        listNode.next.next = new ListNode(2);
+        System.out.println(main.isPalindrome(listNode));
+    }
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (fast != null) {
+            slow = slow.next;
+        }
+        cut(head, slow);
+        ListNode temp = reverse(slow);
+        return isEqual(head, temp);
+    }
+    private ListNode reverse(ListNode head) {
+        ListNode newHead = null;
+        while (head != null) {
+            ListNode nextNode = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = nextNode;
+        }
+        return newHead;
+    }
+    private void cut(ListNode head, ListNode cutNode) {
+        while (head.next != cutNode) {
+            head = head.next;
+        }
+        head.next = null;
+    }
+    private boolean isEqual(ListNode l1, ListNode l2) {
+        while (l1 != null && l2 != null) {
+            if (l1.val != l2.val) return false;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        return true;
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
