a155e453df78d999840f691712ea149f4a7c7d53
dzr <2201357779@qq.com>
2020-07-08 09:06:28 +0800

    leetcode 104

---
+++ src/leetcode/problem104/Main.java
@@ -0,0 +1,40 @@
+package leetcode.problem104;
+
+/**
+ *给定一个二叉树，找出其最大深度。
+ *
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例：
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回它的最大深度 3 。
+ */
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(4);
+        root.right = new TreeNode(3);
+
+        System.out.println(main.maxDepth(root));
+    }
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
