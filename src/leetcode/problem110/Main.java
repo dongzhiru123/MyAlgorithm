cf65e58c6c1ba52c369dd51e6954ca75fa105ee2
dzr <2201357779@qq.com>
2020-07-08 09:18:12 +0800

    leetcode 110

---
+++ src/leetcode/problem110/Main.java
@@ -0,0 +1,57 @@
+package leetcode.problem110;
+
+/**
+ *给定一个二叉树，判断它是否是高度平衡的二叉树。
+ * 本题中，一棵高度平衡二叉树定义为：
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+ *
+ * 示例 1:
+ * 给定二叉树 [3,9,20,null,null,15,7]
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回 true 。
+ *
+ * 示例 2:
+ * 给定二叉树 [1,2,2,3,3,null,null,4,4]
+ *
+ *        1
+ *       / \
+ *      2   2
+ *     / \
+ *    3   3
+ *   / \
+ *  4   4
+ * 返回 false 。
+ */
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.left.left = new TreeNode(4);
+        root.right = new TreeNode(3);
+        System.out.println(main.isBalanced(root));
+    }
+    public boolean isBalanced(TreeNode root) {
+        maxDepth(root);
+        return result;
+    }
+    private boolean result = true;
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int l = maxDepth(root.left);
+        int r = maxDepth(root.right);
+        if (Math.abs(l - r) > 1) result = false;
+        return Math.max(l, r) + 1;
+    }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
