c39bf27f781e0972ac230724678075d28a9adf35
dzr <2201357779@qq.com>
2020-07-10 01:56:47 +0800

    leetcode 530

--- src/leetcode/problem530/Main.java
+++ src/leetcode/problem530/Main.java
@@ -1,7 +1,24 @@
 package leetcode.problem530;
 
 /**
+ *给你一棵所有节点为非负值的二叉搜索树，请你计算树中任意两节点的差的绝对值的最小值。
  *
+ *  
+ *
+ * 示例：
+ *
+ * 输入：
+ *
+ *    1
+ *     \
+ *      3
+ *     /
+ *    2
+ *
+ * 输出：
+ * 1
+ * 解释：
+ * 最小绝对差为 1，其中 2 和 1 的差的绝对值为 1（或者 2 和 3）。
  */
 public class Main {
     public static void main(String[] args) {
