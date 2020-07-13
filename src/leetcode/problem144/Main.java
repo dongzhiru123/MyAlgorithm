f385fe13e60a98d9d51243c64b80bc03803bec1c
dzr <2201357779@qq.com>
2020-07-13 23:23:32 +0800

    leetcode 144

---
+++ src/leetcode/problem144/Main.java
@@ -0,0 +1,52 @@
+package leetcode.problem144;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 给定一个二叉树，返回它的 前序 遍历。
+ *
+ *  示例:
+ *
+ * 输入: [1,null,2,3]
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *
+ * 输出: [1,2,3]
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ */
+public class Main {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        Main main = new Main();
+        List<Integer> list = main.preorderTraversal(root);
+        for (int temp : list) System.out.print(temp + " ");
+    }
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            TreeNode temp = stack.pop();
+            result.add(temp.val);
+            if (temp.right != null) stack.add(temp.right);
+            if (temp.left != null) stack.add(temp.left);
+        }
+        return result;
+    }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
