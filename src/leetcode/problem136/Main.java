cd10e07f8ab4b049638cf51974e3df7f65defbba
dzr <2201357779@qq.com>
2020-07-25 22:56:58 +0800

    leetcode 136

---
+++ src/leetcode/problem136/Main.java
@@ -0,0 +1,28 @@
+package leetcode.problem136;
+
+/**
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ *
+ * 说明：
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ *
+ * 示例 1:
+ * 输入: [2,2,1]
+ * 输出: 1
+ *
+ * 示例 2:
+ * 输入: [4,1,2,1,2]
+ * 输出: 4
+ */
+public class Main {
+    public static void main(String[] args) {
+        Main main = new Main();
+        System.out.println(main.singleNumber(new int[]{1, 1, 2, 3, 2}));
+    }
+    public int singleNumber(int[] nums) {
+        if (null == nums || nums.length == 0) return -1;
+        int result = 0;
+        for (int temp : nums) result ^= temp;
+        return result;
+    }
+}
