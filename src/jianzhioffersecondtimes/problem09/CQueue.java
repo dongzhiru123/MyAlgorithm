053e7878591121d0d520ff713444cdbf7d41bbb2
dzr <2201357779@qq.com>
2020-07-29 15:26:10 +0800

    剑指offer 第九题

---
+++ src/jianzhioffersecondtimes/problem09/CQueue.java
@@ -0,0 +1,43 @@
+package jianzhioffersecondtimes.problem09;
+
+import java.util.Stack;
+
+/**
+ * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+ *
+ * 示例 1：
+ * 输入：
+ * ["CQueue","appendTail","deleteHead","deleteHead"]
+ * [[],[3],[],[]]
+ * 输出：[null,null,3,-1]
+ *
+ * 示例 2：
+ * 输入：
+ * ["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+ * [[],[],[5],[2],[],[]]
+ * 输出：[null,-1,null,null,5,2]
+ *
+ * 提示：
+ * 1 <= values <= 10000
+ * 最多会对 appendTail、deleteHead 进行 10000 次调用
+ */
+public class CQueue {
+
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+
+    public CQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void appendTail(int value) {
+        while (!stack1.isEmpty()) stack2.push(stack1.pop());
+        stack1.push(value);
+        while (!stack2.isEmpty()) stack1.push(stack2.pop());
+    }
+
+    public int deleteHead() {
+        return stack1.isEmpty() ? -1 : stack1.pop();
+    }
+}
