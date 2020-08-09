4c8814e7e7b8c8f52241842c6eb78d6bd0719016
dzr <2201357779@qq.com>
2020-08-09 13:22:33 +0800

    剑指offer 第三十题

---
+++ src/jianzhioffersecondtimes/problem30/MinStack.java
@@ -0,0 +1,60 @@
+package jianzhioffersecondtimes.problem30;
+
+import java.util.Stack;
+
+/**
+ *定义栈的数据结构，请在该类型中实现一个能够得到栈的最小元素的 min 函数在该栈中，调用 min、push 及 pop 的时间复杂度都是 O(1)。
+ *
+ * 示例:
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.min();   --> 返回 -3.
+ * minStack.pop();
+ * minStack.top();      --> 返回 0.
+ * minStack.min();   --> 返回 -2.
+ *  
+ * 提示：
+ * 各函数的调用总次数不超过 20000 次
+ */
+public class MinStack {
+
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        minStack.push(minStack.isEmpty() ? x : (x < minStack.peek() ? x : minStack.peek()));
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int min() {
+        return minStack.peek();
+    }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.min());
+        minStack.pop();
+        System.out.println(minStack.top());
+        System.out.println(minStack.min());
+    }
+}
