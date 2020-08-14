9e2f47aef7eb564fadad02d39261bb5aa1a9ccfe
dzr <2201357779@qq.com>
2020-08-14 17:40:54 +0800

    剑指offer 第三十三题

---
+++ src/jianzhioffersecondtimes/problem33/Main.java
@@ -0,0 +1,41 @@
+package jianzhioffersecondtimes.problem33;
+
+import java.util.Stack;
+
+/**
+ *输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历结果。如果是则返回 true，否则返回 false。假设输入的数组的任意两个数字都互不相同。
+ *
+ * 参考以下这颗二叉搜索树：
+ *
+ *      5
+ *     / \
+ *    2   6
+ *   / \
+ *  1   3
+ *
+ * 示例 1：
+ * 输入: [1,6,3,2,5]
+ * 输出: false
+ *
+ * 示例 2：
+ * 输入: [1,3,2,6,5]
+ * 输出: true
+ *  
+ * 提示：
+ * 数组长度 <= 1000
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    public boolean verifyPostorder(int[] postorder) {
+        Stack<Integer> minStack = new Stack<>();
+        int root = Integer.MAX_VALUE;
+        for (int i = postorder.length - 1; i >= 0; i--) {
+            if (postorder[i] > root) return false;
+            while (!minStack.isEmpty() && postorder[i] < minStack.peek()) root = minStack.pop();
+            minStack.push(postorder[i]);
+        }
+        return true;
+    }
+}
