478858d6c4e86dfd0c51cdda52cd5497454848cf
dzr <2201357779@qq.com>
2020-08-21 14:18:14 +0800

    剑指offer 第四十一题

---
+++ src/jianzhioffersecondtimes/problem41/MedianFinder.java
@@ -0,0 +1,56 @@
+package jianzhioffersecondtimes.problem41;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * 如何得到一个数据流中的中位数？如果从数据流中读出奇数个数值，那么中位数就是所有数值排序之后位于中间的数值。如果从数据流中读出偶数个数值，那么中位数就是所有数值排序之后中间两个数的平均值。
+ *
+ * 例如，
+ * [2,3,4] 的中位数是 3
+ * [2,3] 的中位数是 (2 + 3) / 2 = 2.5
+ *
+ * 设计一个支持以下两种操作的数据结构：
+ *
+ * void addNum(int num) - 从数据流中添加一个整数到数据结构中。
+ * double findMedian() - 返回目前所有元素的中位数。
+ *
+ * 示例 1：
+ * 输入：
+ * ["MedianFinder","addNum","addNum","findMedian","addNum","findMedian"]
+ * [[],[1],[2],[],[3],[]]
+ * 输出：[null,null,null,1.50000,null,2.00000]
+ *
+ * 示例 2：
+ * 输入：
+ * ["MedianFinder","addNum","findMedian","addNum","findMedian"]
+ * [[],[2],[],[3],[]]
+ * 输出：[null,null,2.00000,null,2.50000]
+ */
+public class MedianFinder {
+    public static void main(String[] args) {
+
+    }
+
+    Queue<Integer> A, B;
+
+    /** initialize your data structure here. */
+    public MedianFinder() {
+        A = new PriorityQueue<>(); // 默认小顶堆,用来存放较大的一半数字.
+        B = new PriorityQueue<>((x, y) -> (y - x)); // 大顶堆,用来存放较小的一半数字.
+    }
+
+    public void addNum(int num) {
+        if (A.size() != B.size()) {
+            A.add(num);
+            B.add(A.poll());
+        } else {
+            B.add(num);
+            A.add(B.poll());
+        }
+    }
+
+    public double findMedian() {
+        return A.size() == B.size() ? (A.peek() + B.peek()) / 2.0 : A.peek();
+    }
+}
